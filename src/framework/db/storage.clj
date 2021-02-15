b5a4eacc836a8b4c43fbddb6eeba7ffa86135399
Ian Fernandez <d.ian.b@live.com>
2021-02-15 15:55:40 -0300

    Fix component/Lifecycle to Framework

--- src/framework/db/storage.clj
+++ src/framework/db/storage.clj
@@ -1,16 +1,18 @@
 (ns framework.db.storage
   (:require
-    [next.jdbc :as jdbc]))
+   [com.stuartsierra.component :as component]
+   [next.jdbc :as jdbc]))
 
 (defrecord PostgreSQL
-  [config]
+    [config]
+  component/Lifecycle
   (start [this]
-         (let [datasource (jdbc/get-datasource config)]
-           (-> this
-               (assoc :datasource datasource)
-               (assoc :connection (jdbc/get-connection datasource)))))
+    (let [datasource (jdbc/get-datasource config)]
+      (-> this
+          (assoc :datasource datasource)
+          (assoc :connection (jdbc/get-connection datasource)))))
   (stop [this]
-        (dissoc this :datasource :connection)))
+    (dissoc this :datasource :connection)))
 
 (defn postgresql
   [config]
