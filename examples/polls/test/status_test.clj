6d97135549e94ca8d13f4bea2109994a4f761d26
Bohdan Korniyenko <me@yatagan.xyz>
2021-02-17 12:42:17 +0200

    Test sample

--- examples/polls/test/status_test.clj
+++ examples/polls/test/status_test.clj
@@ -1,15 +1,26 @@
 (ns status-test
   (:require [app]
+            [router]
             [framework.config.core :as config]
+            [com.stuartsierra.component :as component]
             [kerodon.core :refer :all]
             [kerodon.test :refer :all]
             [clojure.test :refer :all]))
 
 (deftest status-test
-  (let [config (config/edn)
+  (let [config  (config/edn)
         app-cfg (:framework.app/ring config)
-        handler (app/ring-app app-cfg)]
-      (-> (session handler)
-          (visit "/status")
-          (has (status? 200))
-          (has (some-regex? "^.*OK.*$")))))
+        handler (->
+                  (component/system-map
+                    :config config
+                    :router (router/make-router)
+                    :app (app/make-app app-cfg))
+                  (component/system-using
+                    {:router []
+                     :app    [:router]})
+                  (component/start-system)
+                  (get-in [:app :handler]))]
+    (-> (session handler)
+        (visit "/re-frame")
+        (has (status? 200))
+        (has (some-regex? "^.*JavaScript.*$")))))
